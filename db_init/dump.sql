dfc6c75e9ab1365d907a064c61ca277e6ca33db8
Bowei <iorange@126.com>
2016-06-19 20:49:48 +0800

    Table `task`

--- db_init/dump.sql
+++ db_init/dump.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `dianwuyou` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `dianwuyou`;
+-- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
 --
 -- Host: localhost    Database: dianwuyou
 -- ------------------------------------------------------
--- Server version	5.7.12-log
+-- Server version	5.7.12
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -41,6 +43,59 @@ LOCK TABLES `file` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) NOT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `gap_duration` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `entrance_type` int(11) DEFAULT NULL,
+  `keyword` varchar(256) DEFAULT NULL,
+  `taokouling` varchar(256) DEFAULT NULL,
+  `act_name` varchar(128) DEFAULT NULL,
+  `act_type` varchar(128) DEFAULT NULL,
+  `rank_by` int(11) DEFAULT NULL,
+  `buyer_gender` int(11) DEFAULT NULL,
+  `buyer_age` int(11) DEFAULT NULL,
+  `buyer_location` varchar(45) DEFAULT NULL,
+  `buyer_rank` int(11) DEFAULT NULL,
+  `payment_amount` double DEFAULT NULL,
+  `sku` varchar(45) DEFAULT NULL,
+  `qty_to_buy` int(11) NOT NULL,
+  `claimed_count` int(11) NOT NULL DEFAULT '0',
+  `require_chat` bit(1) DEFAULT NULL,
+  `page_range` int(11) DEFAULT NULL,
+  `price` double NOT NULL,
+  `shipment_covered` bit(1) NOT NULL,
+  `coupon` double DEFAULT NULL,
+  `coupon_link` varchar(256) DEFAULT NULL,
+  `sec_msg` varchar(256) DEFAULT NULL,
+  `comment_order` varchar(100) DEFAULT NULL,
+  `comment_search` varchar(100) DEFAULT NULL,
+  `comment_pay` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `owner_id` (`owner_id`),
+  KEY `start_time` (`start_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task`
+--
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+INSERT INTO `task` VALUES (1,1,'2016-05-20 13:14:00',10,0,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1.02,NULL,100,0,NULL,NULL,500,'',NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -75,6 +130,7 @@ CREATE TABLE `user` (
   `verify_photo` int(11) DEFAULT NULL,
   `screenshot_img` int(11) DEFAULT NULL,
   `qq` varchar(45) DEFAULT NULL,
+  `reputation` int(11) NOT NULL DEFAULT '0',
   `version` int(11) DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `email` (`email`),
@@ -88,7 +144,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,0,'test@11111111.com','3580b1872023efc4ea8f6290003dd564','dwy_14658846603041815743918','f1d064628d3646929b52db3fae81e18d','15388888888',NULL,0,0,'bl我我',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1);
+INSERT INTO `user` VALUES (1,0,'noone_knows@yeah.net','9b67c99cc8ad34568181e9dd1c102d59','dwy_1466338020995369925333',NULL,'17090830735',NULL,0,0,'noone_knows',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -101,4 +157,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-14 15:56:11
+-- Dump completed on 2016-06-19 20:48:42
